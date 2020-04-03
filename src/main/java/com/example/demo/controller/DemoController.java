62cec80ee47ff210375add924521de4fc777a499
dw113073 <dw113073@alibaba-inc.com>
2020-04-03 17:20:06 +0800

    up

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,19 +1,28 @@
 package com.example.demo.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author dengwei
  * @date 2020/04/03
  */
-@Controller
+@RestController
 public class DemoController {
 
-    @RequestMapping(name = "hello")
-    @ResponseBody
-    public String test() {
-        return "Hello, spring boot";
+    @RequestMapping("/hello")
+    public String say() {
+        System.out.println("Hello springboot");
+        return "hello,this is a springboot demo！~";
     }
+
+    @RequestMapping("/login")
+    public String login(@RequestParam("username") String username,@RequestParam("password") String password){
+        String result= "username:"+username+",password:"+password;
+        System.out.println(result);
+        return(result);
+    }
+
 }
